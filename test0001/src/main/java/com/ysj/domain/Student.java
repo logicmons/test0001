22cfa743b4ced9b3002e7a23b66e7afe7c3c11c4
ysj <jiayouysj@gmail.com>
2020-04-14 22:04:19 +0800

    init

---
+++ test0001/src/main/java/com/ysj/domain/Student.java
@@ -0,0 +1,18 @@
+package com.ysj.domain;
+
+/**
+ * @Author:
+ * @Created Date:10:01 下午 2020/4/14
+ * @Description:
+ */
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
